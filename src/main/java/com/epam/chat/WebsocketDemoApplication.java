333d96826ecfe0465c46999797d0f38906f54c58
Rakesh Gupta <rakeshgupta.contact@gmail.com>
2017-12-23 22:05:52 +0530

    Main app to start chat application

---
+++ src/main/java/com/epam/chat/WebsocketDemoApplication.java
@@ -0,0 +1,16 @@
+package com.epam.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rakeshgupta on 16/12/17.
+ */
+
+@SpringBootApplication
+public class WebsocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketDemoApplication.class, args);
+	}
+}
