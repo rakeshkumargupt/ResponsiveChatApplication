044b29990bfd5a54df578ef7c8a4a1f59362f17a
Rakesh Gupta <rakesh@debian.domain>
2017-12-23 20:37:35 +0530

    Web Socket configuration

---
+++ src/main/java/com/epam/chat/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.epam.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+/**
+ * Created by rakeshgupta on 16/12/17.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/channel");
+    }
+}
